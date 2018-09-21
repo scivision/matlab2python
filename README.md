9172ddb6c5c2805c554e595d094a0976d425d82e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-09-21 14:01:07 -0400

    Using scipy.ndimage for simplicity
    R2018b working
    
    doc

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # matlab2python
-Examples of Matlab >= R2014b calling Python functions
+Examples of Matlab &ge; R2018b calling Python functions.
+Uses the new R2018b `memoryview` to efficiently transfer large variables without copying, as Matlab R2014b..R2018a had required.
+
+## Setup
+
+Matlab R2018b
+[supports Python versions]([supports Python versions](https://www.mathworks.com/help/matlab/matlab_external/system-requirements-for-matlab-engine-for-python.html)) 
+2.7, 3.5 and 3.6.
+Thus you may need to setup Python 3.6 if your usual workflow is with Python 3.7 etc.
+
+1. create Python 3.6 virtual environment (assuming Anaconda Python)
+   ```sh
+   conda create -n py36 python=3.6
+   ```
+2. set Matlab to Python 3.6 by restarting Matlab and then in Matlab
+   ```matlab
+   pyversion ~/miniconda3/envs/py36/bin/python
+   ```
+3. verify Matlab Python config by typing `pyversion`:
+   ```
+        version: '3.6'
+    executable: '~/miniconda3/envs/py36/bin/python'
+       library: '~/miniconda3/envs/py36/lib/libpython3.6m.so'
+          home: '~/miniconda3/envs/py36'
+      isloaded: 0
+   ```
+
--- archive/image_proc_1D.m
+++ archive/image_proc_1D.m
@@ -1,21 +1,23 @@
 %% example of doing image processing in Python with image from Matlab
 % assumes that you have previously installed scipy in Python.
-assert(~verLessThan('matlab', '9.5'), 'Matlab >= R2018b required')
+assert(~verLessThan('matlab', '8.4'), 'Matlab >= R2014b required')
 %% 200x320 image 
 dat = load('clown');
 img = dat.X;
+%% ravel X to a row vector, and unravel with Numpy
+Xp = py.numpy.reshape(img(:)', uint16(size(img)),'F'); 
 %% Apply Gaussian filter to image
-% Yp = py.skimage.filters.gaussian(Xp, 3);  % segfaults R2018b with Python 3.6
-Y = py.scipy.ndimage.gaussian_filter(img, 3);
-%% convert from Numpy array to Matlab array
-Y = double(Y); 
+% Yp = py.skimage.filters.gaussian(Xp, 3);
+Yp = py.scipy.ndimage.gaussian_filter(Xp, 3);
+%% Y is a regular Matlab 2-D matrix
+Y = reshape(cell2mat(cell(Yp.ravel('F').tolist())), size(img)); 
 %% plot
 figure(1), clf(1)
 subplot(1,3,1), imshow(img, dat.map),title('original')
 subplot(1,3,2), imshow(Y, dat.map),title('filtered in Python')
 %% now let's do something similar in Matlab
 % the filter truncation radius isn't the same, so the numerical results differ.
-F = fspecial('gaussian', [15,15], 3);
+F = fspecial('gaussian',[15,15], 3);
 M = imfilter(img, F);
 
 subplot(1,3,3), imshow(M, dat.map), title('Matlab filtered')
